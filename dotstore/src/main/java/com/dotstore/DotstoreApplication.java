045ae14feab3e07b5054942ff03de10bd0a3a7be
Sourabh Sanjay Hawale <69086016+Sourabh-1008@users.noreply.github.com>
2025-06-21 18:49:52 +0530

    Initial commit

---
+++ dotstore/src/main/java/com/dotstore/DotstoreApplication.java
@@ -0,0 +1,13 @@
+package com.dotstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DotstoreApplication.class, args);
+	}
+
+}
